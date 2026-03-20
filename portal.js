61048706405ccc4d4213cb00016aad0d8ccf0735
lilgordong <98044737+lilgordong@users.noreply.github.com>
2026-03-20 13:06:31 -0400

    Delete portal.js

--- portal.js
+++
@@ -1,7 +0,0 @@
-function portalLogin(){
-let email=document.getElementById("portalEmail").value;
-let pass=document.getElementById("portalPass").value;
-if(email&&pass){alert("Login successful (demo)");}else{alert("Enter credentials");}
-}
-function uploadDocs(){alert("Documents uploaded (demo)");}
-function sendMessage(){let msg=document.getElementById("clientMessage").value; if(msg){alert("Message sent!");}else{alert("Enter a message");}}
