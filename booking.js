d2b6f9bfdf26280a23ff753c239b82430306e2f6
lilgordong <98044737+lilgordong@users.noreply.github.com>
2026-03-20 13:05:01 -0400

    Delete booking.js

--- booking.js
+++
@@ -1,6 +0,0 @@
-function bookAppointment(){
-let date=document.getElementById("apptDate").value;
-let time=document.getElementById("apptTime").value;
-if(!date){alert("Select a date"); return;}
-alert(`Appointment booked on ${date} at ${time}. We will contact you shortly.`);
-}
