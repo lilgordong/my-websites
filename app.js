928cdc0af4dc29c21c25082309ace074e8666a35
lilgordong <98044737+lilgordong@users.noreply.github.com>
2026-03-20 14:06:43 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,3 @@
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-
-/* REGISTER */
 window.registerUser = function(email, password) {
-  createUserWithEmailAndPassword(window.auth, email, password)
-    .then(() => {
-      alert("Account created!");
-      window.location.href = "dashboard.html";
-    })
-    .catch(err => alert(err.message));
-};
-
-/* LOGIN */
-window.loginUser = function(email, password) {
-  signInWithEmailAndPassword(window.auth, email, password)
-    .then(() => {
-      alert("Logged in!");
-      window.location.href = "dashboard.html";
-    })
-    .catch(err => alert(err.message));
-};
-
-/* LOGOUT */
-window.logoutUser = function() {
-  signOut(window.auth).then(() => {
-    window.location.href = "login.html";
-  });
+  alert("Button works. Email: " + email);
 };
